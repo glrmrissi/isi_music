6448f2c9fffee8b6c4d904152b25082b423fd9ee
glrmrissi <guilhermerissi04112006@gmail.com>
2026-06-11 19:10:52 -0300

    feat: add OptionsPanel keybinding (T key)

--- src/keybinds/mod.rs
+++ src/keybinds/mod.rs
@@ -43,6 +43,7 @@ pub enum Action {
     ScrollUp,
     ScrollDown,
     Quit,
+    OptionsPanel,
 }
 
 impl Action {
@@ -83,6 +84,7 @@ impl Action {
             ("scroll_up", &["pageup"], ScrollUp),
             ("scroll_down", &["pagedown"], ScrollDown),
             ("quit", &["q", "ctrl+c"], Quit),
+            ("options_panel", &["t"], OptionsPanel),
         ]
     }
 }
@@ -436,11 +438,12 @@ impl Keybinds {
                     Action::Help,
                     Action::ToggleCompact,
                     Action::ToggleFullscreen,
-                    Action::ToggleVisualizer,
-                    Action::ToggleLyrics,
-                    Action::ToggleDebug,
-                    Action::ScrollUp,
-                    Action::ScrollDown,
+                Action::ToggleVisualizer,
+                Action::ToggleLyrics,
+                Action::ToggleDebug,
+                Action::ScrollUp,
+                Action::ScrollDown,
+                Action::OptionsPanel,
                 ],
             ),
             (
@@ -536,7 +539,8 @@ impl KeybindsTomlOutput {
                 | Action::ToggleLyrics
                 | Action::ToggleDebug
                 | Action::ScrollUp
-                | Action::ScrollDown => modes.push(entry),
+                | Action::ScrollDown
+                | Action::OptionsPanel => modes.push(entry),
                 Action::AddToQueue
                 | Action::RemoveFromQueue
                 | Action::SortTracks
